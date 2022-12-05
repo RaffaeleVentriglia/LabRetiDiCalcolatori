7a44f7918679103c6ae9a3ee511033fd7bcc871b
RaffaeleVentriglia <raffaeleventriglia6@gmail.com>
2022-12-06 00:15:24 +0100

    inserted functions for server and client

--- GreenPass/function.c
+++ GreenPass/function.c
@@ -8,3 +8,137 @@ int Socket(int family, int type, int protocol) {
     }
     return (n);
 }
+
+void Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+    if(connect(sockfd, addr, addrlen) < 0) {
+        perror("connect");
+        exit(1);
+    }
+}
+
+void Close(int fd) {
+    if(close(fd) < 0) {
+        perror("close");
+        exit(-1);
+    }
+}
+
+void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+    if(bind(sockfd, addr, addrlen) < 0) {
+        perror(" bind");
+        exit(1);
+    }
+}
+
+void Listen(int sockfd, int backLog) {
+    if(listen(sockfd, backLog) < 0) {
+        perror("listen");
+        exit(1);
+    }
+}
+
+int Accept(int sockfd, struct sockaddr *addr, socklen_t addrlen) {
+    int connfd;
+    if((connfd = accept(sockfd, addr, addrlen)) < 0) {
+        perror("accept");
+        exit(1);
+    }
+    return connfd;
+}
+
+void IPconversion(int af, const char *src, struct in_addr *dst) {
+    if(inet_pton(AF_INET, src, dst) < 0) {
+        fprintf(stderr, "inet_pton error: %s\n", src);
+        exit(1);
+    }
+}
+
+void *GetCurrentDir(void) {
+    char *CurrWorkDir, *token, *directory;
+    char buffer[MAXPATH + 1];
+    size_t length;
+
+    CurrWorkDir = getcwd(buffer, MAXPATH + 1);
+    if(CurrWorkDir == NULL) {
+        perror("getcwd");
+        exit(1);
+    }
+
+    token = strrchr(CurrWorkDir, '/');
+    if(token == NULL) {
+        fprintf(stderr, "Directory mancante");
+        exit(1);
+    }
+
+    length = strlen(token);
+    directory = malloc(length);
+    if(directory == NULL) {
+        fprintf(stderr, "Errore nella malloc");
+        exit(1);
+    }
+    memcpy(directory, token + 1, length);
+    return directory;
+}
+
+void Chdir(const char *path) {
+    if(strcmp((const char *)GetCurrentDir(), path) != 0) {
+        if(chdir(path) == -1) {
+            perror("chdir");
+            exit(1);
+        }
+    }
+}
+
+void Remove(const char *pathname) {
+    if(remove(pathname) == -1) {
+        perror("remove");
+        exit(1);
+    }
+}
+
+void Rename(const char *oldpath, const char *newpath) {
+    if(rename(oldpath, newpath) == -1) {
+        perror("remove");
+        exit(1);
+    }
+}
+
+ssize_t FullWrite(int fd, void *buf, size_t count) {
+    size_t nleft;
+    ssize_t nwritten;
+    nleft = count;
+    while(nleft > 0) {
+        if((nwritten = write(fd, buf, count)) < 0) {
+            if(errno == EINTR)
+                continue;
+            else {
+                perror("write");
+                exit(nwritten);
+            }
+        }
+        nleft -= nwritten;
+        buf += nwritten;
+    }
+    return nleft;
+}
+
+ssize_t FullRead(int fd, void *buf, size_t count) {
+    size_t nleft;
+    ssize_t nread;
+    nleft = count;
+    while(nleft > 0) {
+        if((nread = read(fd, buf, count)) < 0) {
+            if(errno == EINTR)
+                continue;
+            else {
+                perror("read");
+                exit(1);
+            }
+        } else if(nread == 0)
+            break;
+        nleft -= nread;
+        buf += nread;
+    }
+    buf = 0;
+    return nleft;
+}
--- GreenPass/function.h
+++ GreenPass/function.h
@@ -13,18 +13,22 @@
 #include <ctype.h>
 #include <sys/select.h>
 
-int Socket(int family, int type, int protocol);
-void Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
-void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
-void Listen(int sockfd, int backlog);
-int Accept(int sockfd, struct sockaddr *addr, socklen_t addrlen);
-void IPconversion(int af, const char *src, struct in_addr *dst);
-void Close(int fd);
+#define MAXLINE 256
+#define QUEUESIZE 1024
+#define MAXPATH 4096
+
+int Socket(int, int, int);
+void Connect(int, const struct sockaddr *, socklen_t);
+void Bind(int, const struct sockaddr *, socklen_t);
+void Listen(int, int);
+int Accept(int, struct sockaddr *, socklen_t);
+void IPconversion(int, const char *, struct in_addr *);
+void Close(int);
 void *GetCurrentDir(void);
-void Chdir(const char *path);
-void Remove(const char *pathname);
-void Rename(const char *oldpath, const char *newpath);
-ssize_t FullWrite(int fd, void *buf, size_t count);
-ssize_t FullRead(int fd, void *buf, size_t count);
+void Chdir(const char *);
+void Remove(const char *);
+void Rename(const char *, const char *);
+ssize_t FullWrite(int, void *, size_t);
+ssize_t FullRead(int, void *, size_t);
 
 #endif
